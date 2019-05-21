50b707ff46c42a219ce5220e1ab6ee45d751bad5
CYNTHIA BRAINOO <c.brainoo@ymail.com>
2019-05-21 04:41:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, ${Neil deGrasse Tyson}! +${3+2}!
 
-var topic = "Hello everyone";
+var topic = "    ";
 
 var conversation = "${3 +2}";
